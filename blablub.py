453f2113334aa51476384031afc6dcc32780a139
s0wx <s0wx@pm.me>
2022-04-26 17:43:43 +0200

    added hashing of certs

--- blablub.py
+++ blablub.py
@@ -1,8 +1,9 @@
 import pyshark
+import hashlib
 
 
-def extract_tls_certificate_as_bytes(packet):
-    cert_hex = packet._all_fields["tls.handshake.certificate"].split(":")
+def extract_tls_certificate_as_bytes(captured_packet):
+    cert_hex = captured_packet._all_fields["tls.handshake.certificate"].split(":")
     return bytes.fromhex("".join(cert_hex))
 
 
@@ -13,7 +14,13 @@ def save_certificate(certificate_data: bytes, file_path: str):
 
 def extract_tls_cert_packets_from_file(file_path):
     capture = pyshark.FileCapture(file_path, display_filter="ssl.handshake.type==11")
-    return [packet.tls for packet in capture if hasattr(packet, 'tls')]
+    return [capture_packet.tls for capture_packet in capture if hasattr(capture_packet, 'tls')]
+
+
+def calculate_sha256_from_bytes(byte_data):
+    sha256_hash = hashlib.sha256()
+    sha256_hash.update(byte_data)
+    return sha256_hash.hexdigest()
 
 
 if __name__ == '__main__':
@@ -22,3 +29,4 @@ if __name__ == '__main__':
     for cert_num, packet in enumerate(tls_cert_packets):
         cert_data = extract_tls_certificate_as_bytes(packet)
         save_certificate(cert_data, f"cert_{cert_num}.crt")
+        print("hash", cert_num, calculate_sha256_from_bytes(cert_data))
