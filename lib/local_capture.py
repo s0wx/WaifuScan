bbb2a5e4c94f9f8a6e54293b39d7fe402431f876
s0wx <s0wx@pm.me>
2022-04-28 01:19:57 +0200

    added more certificate extensions

--- lib/local_capture.py
+++ lib/local_capture.py
@@ -2,7 +2,20 @@ import os
 
 
 def system_cert_crawl(start_path="."):
+    certificate_extensions = [
+        ".pem",
+        ".crt"
+        ".ca-bundle",
+        ".p7b",
+        ".p7s",
+        ".der",
+        ".cer",
+        ".pfx",
+        ".p12"
+    ]
+
     for root_path, dirs, files in os.walk(start_path):
         for file in files:
-            if file.endswith(".pem"):
-                print(os.path.join(root_path, file))
+            for ext in certificate_extensions:
+                if file.endswith(ext):
+                    print(os.path.join(root_path, file))
