ad56c98ab04dd85ab4a877fa846b619c0e4d9cbb
s0wx <s0wx@pm.me>
2022-04-28 02:55:44 +0200

    runtime tracking

--- lib/local_capture.py
+++ lib/local_capture.py
@@ -1,8 +1,13 @@
+import json
 import os
 import subprocess
+import time
+from collections import defaultdict
 
 
 def system_cert_crawl(start_path="."):
+    start = time.time()
+    counting = defaultdict(int)
     certificate_extensions = [
         ".pem",
         ".crt"
@@ -21,9 +26,13 @@ def system_cert_crawl(start_path="."):
                 if file.endswith(ext):
                     file_type_command = subprocess.check_output("file " + os.path.join(root_path, file).replace(' ', '\ '), shell=True)
                     file_info = file_type_command.decode("utf-8").split(":")[-1].strip()
-
-                    print(file, file_info)
+                    counting[file_info.lower()] += 1
+                    stopped = time.time() - start
+                    print(f"{int(stopped)}s", file, file_info)
                     if "certificate" in file_info.lower():
                         print("CERTIFICATE\n")
                     elif "key" in file_info.lower():
                         print("KEY\n")
+                    print(json.dumps(dict(counting), indent=2))
+                    print(f"{int(stopped)}s for {sum(counting.values())} checks")
+    print(f"TOTAL: {int(time.time() - start)} seconds for {sum(counting.values())} checks")
