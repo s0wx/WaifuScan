f65c2f68f206714131875da446b2a72e6711c289
DominikusMaximus <104563039+DominikusMaximus@users.noreply.github.com>
2022-04-28 14:18:52 +0200

    Add files via upload

---
+++ gui.py
@@ -0,0 +1,48 @@
+#! /usr/bin/python3
+from tkinter import *
+import lib.localcheck
+
+class Window(Frame):
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+
+        # widget can take all window
+        self.pack(fill=BOTH, expand=1)
+
+        localCheckButton = Button(self, text="check local certificates", command=self.clickLocalCheckButton)
+        localCheckButton.place(x=0, y=0)
+
+    def clickExitButton(self):
+        exit()
+
+    def clickLocalCheckButton(self):
+
+        lib.localcheck.localcheck()
+
+
+    def clickMonitorNetworkCerts(self):
+        return
+
+if __name__ == '__main__':
+    root = Tk()
+    app = Window(root)
+    root.wm_title("CertFinder+")
+    root.geometry("500x500")
+    root.mainloop()
+
+
+'''class Window(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+
+
+if __name__ == '__main__':
+
+    root = Tk()
+    app = Window(root)
+    root.wm_title("Certfinder+")
+    root.mainloop()'''
+
